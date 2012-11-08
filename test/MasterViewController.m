8238d5d16891d375d55067824408c07213ab94c2
YOTEST <facetest@live.jp>
2012-11-08 12:12:21 +0900

    1.2
    Ver1.2 test

--- test/MasterViewController.m
+++ test/MasterViewController.m
@@ -29,6 +29,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //
 	// Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
