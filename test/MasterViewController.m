13e58a76566d7f73889e492c8706278acd172894
YOTEST <facetest@live.jp>
2012-11-08 12:06:50 +0900

    1.0
    Ver1.0サンプル

--- test/MasterViewController.m
+++ test/MasterViewController.m
@@ -32,6 +32,10 @@
 	// Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
+    UILabel *label = [[UILabel alloc] init];
+    label.text = @"test";
+    [label ];
+    
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
